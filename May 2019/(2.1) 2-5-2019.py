4d54c2bc52206166baac20e0dd23a14be7defefd
Gern-Yataro <teoh6g@gmail.com>
2019-05-02 15:52:37 +0800

    #3 Insertion Sort

--- May 2019/(2.1) 2-5-2019.py
+++ May 2019/(2.1) 2-5-2019.py
@@ -2,11 +2,9 @@ numbers = [4, 1, 6, 5, 3, 2, 9, 7, 8]
 
 def insertion_sort(array):
   for index in range(1, len(array)):
-    if array[index] < array[index-1]:
-      for y in range(0, index):
-        if array[y] > array[index]:
-          array.insert(y, array.pop(index))   
-          break;
+    while array[index] < array[index-1] and index>0:
+       array[index], array[index-1] = array[index-1], array[index]
+       index -= 1
 
   for z in array:
     print(z, end=" ")
