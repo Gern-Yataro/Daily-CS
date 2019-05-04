d48cf475a6fde86463d48892639fbb2a1eae6728
Gern-Yataro <teoh6g@gmail.com>
2019-05-04 17:04:40 +0800

    #4 Quick Sort

--- May 2019/(3.0) 3-5-2019.py
+++ May 2019/(3.0) 3-5-2019.py
@@ -1,23 +1,25 @@
-numbers = [5, 3, 9, 8, 1, 7, 2, 4, 6]
+numbers = [5, 6, 9, 2, 4, 7, 8, 1, 3]
 
 def quick_sort(array):
-	if len(array) == 1 or len(array) == 0:
-		return array
-	
-	pivot = 0
-	left_point = 1
-	right_point = len(array)-1
-	while left_point < right_point:
-		while array[left_point] < array[pivot]:
-			left_point += 1
-		while array[right_point] > array[pivot]:
-			right_point -= 1
-		if left_point < right_point:
-			array[left_point], array[right_point] = array[right_point], array[left_point]
-	array[pivot], array[right_point] = array[right_point], array[pivot]
-	pivot = right_point
-	temparr1 = quick_sort(array[:right_point])
-	temparr2 = quick_sort(array[right_point+1:])
-	return temparr1 + [array[pivot]] + temparr2
-	
+  if len(array)==1 or len(array)==0:
+    return array
+  left = -1
+  right = len(array)
+  pivot = array[right-1]
+
+  #Sweep through array, if a value smaller than pivot is found, move to leftmost of array, in front of previously smaller than pivot values
+  for x in range(0, right):
+    if array[x] < pivot:
+      left += 1
+      array[x], array[left] = array[left], array[x]
+
+  #Move pivot value to the front of all values smaller than pivot
+  array[right-1], array[left+1] = array[left+1], array[right-1]
+
+  #Split array into before and after pivot and call sort recursively
+  temparr1 = quick_sort(array[:left+1])
+  temparr2 = quick_sort(array[left+2:])
+  
+  return temparr1 + [pivot] + temparr2
+
 print(quick_sort(numbers))
